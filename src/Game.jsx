10731b91db3b35666d8360323f14cb8d7e562248
Vlad Cojanu <cojanu.vlad@gmail.com>
2019-03-12 22:04:59 +0000

    - Added game logic - Added random board generation - Fixed bug relating to board size

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,7 +3,7 @@ import './Game.css';
 
 const CELL_SIZE = 20;
 const GAME_WIDTH = 1200;
-const GAME_HEIGHT = 600;
+const GAME_HEIGHT = 800;
 
 // TODO: Change cell into function component
 class Cell extends React.Component {
@@ -23,14 +23,36 @@ class Cell extends React.Component {
 class Game extends React.Component {
     constructor() {
         super();
-        this.rows = GAME_WIDTH / CELL_SIZE;
+        this.rows = GAME_HEIGHT / CELL_SIZE;
         this.cols = GAME_WIDTH / CELL_SIZE;
+        console.log(this.cols);
+        console.log(this.rows);
         this.board = this.makeEmptyBoard();
     }
     state = {
         cells: [],
         interval: 100,
         isRunning: false,
+        seed : 42,
+    }
+
+    // Counts the neighboors of a cell located at x, y given a board of cells.
+    calculateNeighbors(board, x, y) {
+        let neighbors = 0;
+        const dirs = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+        for (let i = 0; i < dirs.length; i++) {
+            const dir = dirs[i];
+            let yNeighboor = y + dir[0];
+            let xNeightboor = x + dir[1];
+
+            // bounds check then value check
+            if (xNeightboor >= 0 && xNeightboor < this.cols && yNeighboor >= 0 && yNeighboor < this.rows 
+                && board[yNeighboor][xNeightboor]) {
+                neighbors++;
+            }
+        }
+
+        return neighbors;
     }
 
     runGame = () => {
@@ -49,7 +71,25 @@ class Game extends React.Component {
     runIteration() {
         console.log('running iteration');
         let newBoard = this.makeEmptyBoard();
-        // TODO: Add logic for each iteration here.
+
+        // Game Logic
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                let neighbors = this.calculateNeighbors(this.board, x, y);
+                if (this.board[y][x]) {
+                    if (neighbors === 2 || neighbors === 3) {
+                        newBoard[y][x] = true;
+                    } else {
+                        newBoard[y][x] = false;
+                    }
+                } else {
+                    if (!this.board[y][x] && neighbors === 3) {
+                        newBoard[y][x] = true;
+                    }
+                }
+            }
+        }
+
         this.board = newBoard;
         this.setState({ cells: this.makeCells() });
         this.timeoutHandler = window.setTimeout(() => {
@@ -59,6 +99,37 @@ class Game extends React.Component {
 
     handleIntervalChange = (event) => {
         this.setState({ interval: event.target.value });
+    }
+
+    handleSeedChange = (event) => {
+        console.log('Setting seed to: ' + event.target.value);
+        this.setState({seed : event.target.value});
+    }
+    
+    mulberry32 = (a) => {
+        return function() {
+          var t = a += 0x6D2B79F5;
+          t = Math.imul(t ^ t >>> 15, t | 1);
+          t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+          return ((t ^ t >>> 14) >>> 0) / 4294967296;
+        }
+    }
+
+    seedGame = () => {
+        let newBoard = this.makeEmptyBoard();
+        
+        var prng = this.mulberry32(this.state.seed);
+
+        for(let y = 0; y < this.rows; y++){
+            for(let x = 0; x < this.cols; x++){
+                var i = Math.floor(prng() * 1337);
+                console.log('i is:' + i);
+                newBoard[y][x] = ((i % 7) === 0);
+            }
+        }
+
+        this.board = newBoard;
+        this.setState({ cells: this.makeCells() });
     }
 
     // Create a 2d array represenation of the board 
@@ -131,6 +202,10 @@ class Game extends React.Component {
                         <button className="button"
                             onClick={this.runGame}>Run</button>
                     }
+                First generation seed <input value={this.state.seed}
+                        onChange={this.handleSeedChange} />
+                <button className="button"
+                        onClick={this.seedGame}>Seed Game</button>
                 </div>
             </div>
         );
